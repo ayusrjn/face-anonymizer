783bc40ad428fc31fcc729ccbdce5bf453a8da8f
ayush <ayushraanjan@gmail.com>
2025-11-16 13:33:03 +0530

    added support for directory  + added progress bar

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,8 @@
 import cv2
 import argparse, shutil, os
 from yolo_infer import predict
+from tqdm import tqdm
+import math
 
 from blur import (
     gaussian_blur,
@@ -25,6 +27,7 @@ def parse_args():
     group = ap.add_mutually_exclusive_group(required=True)
     group.add_argument("--img", help="Path to the imput image file")
     group.add_argument("--vid", help="Path to the video file")
+    group.add_argument("--dir", help="Path to the Folder where the images are")
 
     ap.add_argument(
         "--kernel",
@@ -88,7 +91,7 @@ def process_frame(frame, detect_results, blur_fn, kernel_tuple,kernel_int):
         return blur_fn(frame, detect_results, kernel_int)
 
 
-def run_on_image(img_path, blur_name, kernel_val):
+def run_on_image(img_path, blur_name, kernel_val, output_path):
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     if img is None:
         raise FileNotFoundError(f"Could not open image :{img_path}")
@@ -101,15 +104,41 @@ def run_on_image(img_path, blur_name, kernel_val):
     out = process_frame(img, detect_results,blur_fn,kernel_tuple, kernel_int)
 
     filename_base = os.path.splitext(os.path.basename(img_path))[0]
-    cv2.imwrite(f"{filename_base}_blurred.jpg", out)
 
-    print(f"Saved blurred image ")
+
+    if out is not None:
+        directory_to_save = output_path + "/face_anonymized"
+        os.makedirs(directory_to_save, exist_ok=True)
+        save_path = directory_to_save + f"/{filename_base}_blurred.jpg"
+        cv2.imwrite(str(save_path), out)
+
+        print(f"Blurred Image at : {save_path}")
+
+    else:
+        cv2.imwrite(f"{filename_base}_blurred.jpg", out)
+        print(f"Saved blurred image  :{filename_base}")
+
+    
+
+def run_on_dir(dir_path, blur_name, kernel_val):
+
+    if not os.path.isdir(dir_path):
+        raise FileNotFoundError(f"Could not the Directory :{dir_path}")
+
+    for filename in tqdm(os.listdir(dir_path), desc="Processing Images"):
+        if filename.lower().endswith(('.png', '.jpg', 'jpeg')):
+            img_path = os.path.join(dir_path, filename)
+            run_on_image(img_path, blur_name, kernel_val, dir_path)
+
+    print(f"Successful Run in Directory : {dir_path}")
 
 def run_on_video(video_path, blur_name, kernel_val):
 
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         raise IOError(f"Cannot open video: {video_path}")
+
+    total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT) or 0)
 
     fps = cap.get(cv2.CAP_PROP_FPS)
     if fps is None or fps <=1:
@@ -132,19 +161,34 @@ def run_on_video(video_path, blur_name, kernel_val):
     kernel_tuple = (kernel_val, kernel_val) if expects_tuple else None
     kernel_int = kernel_val if expects_int else None
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+    progress_bar = tqdm(total_frame if total_frame > 0 else None,
+                        desc=f"Processing Video {video_path}",
+                        unit=" frame", ncols=100)
 
-        results = predict(frame)
-        processed_frame = process_frame(frame, results, blur_fn, kernel_tuple, kernel_int)
+    frame_idx = 0
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        writer.write(processed_frame)
+            results = predict(frame)
+            processed_frame = process_frame(frame, results, blur_fn, kernel_tuple, kernel_int)
 
-    cap.release()
-    writer.release()
-    print(f"Saved Blurred Video -> {output_file}")
+            writer.write(processed_frame)
+
+            frame_idx += 1
+            progress_bar.update(1)
+
+            if frame_idx % 30 == 0:
+                progress_bar.set_postfix({"fps" : math.floor(fps)})
+
+    finally:
+        progress_bar.close()
+
+        cap.release()
+        writer.release()
+        print(f"Saved Blurred Video -> {output_file}")
 
 
 if __name__ == "__main__":
@@ -154,5 +198,8 @@ if __name__ == "__main__":
 
     if args.img:
         run_on_image(args.img, blur_choice, kernel_val)
+    if args.dir:
+        run_on_dir(args.dir, blur_choice, kernel_val)
     if args.vid:
         run_on_video(args.vid, blur_choice, kernel_val)
+
